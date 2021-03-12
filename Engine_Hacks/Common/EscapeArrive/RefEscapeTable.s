cfee405d3a6a22d53ef7e9cc2dfd54887b60f4c2
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2021-03-12 13:16:15 -0800

    qol updates (newCHAI, walking sounds fix)

--- Engine_Hacks/Common/EscapeArrive/RefEscapeTable.s
+++ Engine_Hacks/Common/EscapeArrive/RefEscapeTable.s
@@ -1,19 +1,31 @@
 .thumb
 .align 4
 
-push {r14}
-ldr r0,SVAL
-ldr r2,SVAL
-ldrb r0,[r0]
-@r0 = char ID
-mov r1,#2
-mul r0,r1
-ldr r1,TextTable
-add r0,r1
-ldrh r0,[r0]
-strh r0,[r2]
-pop {r1}
-bx r1
+RefEscapeTable:
+	push {r14}
+	ldr r0,SVAL
+	ldr r2,SVAL
+	ldrb r0,[r0]
+	@r0 = char ID
+	ldr r1,TextTable
+	FindEscapeQuote:
+		ldrb r3, [r1]
+		cmp r3, #0x0
+		beq NotFound
+			cmp r0, r3
+			bne LoopNext
+				ldrh r0, [r1, #0x2]
+				cmp r0, #0x0
+				bne Found
+	LoopNext:
+		add r1, #0x4
+		b FindEscapeQuote
+NotFound:
+	mov r0, #0x0
+Found:
+	strh r0,[r2]
+	pop {r1}
+	bx r1
 
 .ltorg
 .align 4
