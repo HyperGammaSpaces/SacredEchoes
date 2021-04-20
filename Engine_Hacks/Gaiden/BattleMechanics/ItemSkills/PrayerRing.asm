a1026b7f5b7b58ca736e7fac7f39eb97ae0db519
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2021-04-19 22:49:23 -0700

    make defensive skill anims work again

--- Engine_Hacks/Gaiden/BattleMechanics/ItemSkills/PrayerRing.asm
+++ Engine_Hacks/Gaiden/BattleMechanics/ItemSkills/PrayerRing.asm
@@ -9,51 +9,63 @@
 @luck x 2% activation chance
 .equ d100Result, 0x802a52c
 @ r0 is attacker, r1 is defender, r2 is current buffer, r3 is battle data
-push {r4-r7,lr}
-mov r4, r0 @attacker
-mov r5, r1 @defender
-mov r6, r2 @battle buffer
-mov r7, r3 @battle data
-ldr     r0,[r2]           @r0 = battle buffer
-lsl     r0,r0,#0xD     
-lsr     r0,r0,#0xD        @Without damage data
-mov r1, #2 @miss
-tst r0, r1
-bne End
 
-@check defender's hp >1
-ldrb r1, [r5, #0x13] @current hp
-cmp r1, #1 @1hp left?
-ble End
+Miracle:
+	push {r4-r7,lr}
+	mov r4, r0 @attacker
+	mov r5, r1 @defender
+	mov r6, r2 @battle buffer
+	mov r7, r3 @battle data
+	ldr     r0,[r2]           @r0 = battle buffer
+	lsl     r0,r0,#0xD     
+	lsr     r0,r0,#0xD        @Without damage data
+	mov r1, #2 @miss
+	tst r0, r1
+	bne End
 
-@check damage >= currhp
-mov r0, #4
-ldsh r0, [r7, r0]
-cmp r0, r1
-blt End @not gonna die
+	@check defender's hp >1
+	ldrb r1, [r5, #0x13] @current hp
+	cmp r1, #1 @1hp left?
+	ble End
 
-@check for Miracle
-mov r0, r5 @defender data
-add r0, #0x1E
-mov r2, #0x0
-ItemCheckLoop:
-	ldrb r1, [r0, r2]
-	cmp r1, #PrayerRingID
-	beq DoMiracle
-	add r2, #0x2
-	cmp r2, #0xA
-	blt ItemCheckLoop
-b End
+	@check damage >= currhp
+	mov r0, #4
+	ldsh r0, [r7, r0]
+	cmp r0, r1
+	blt End @not gonna die
 
-DoMiracle:
-@and set damage to currhp-1
-ldrb r0, [r5, #0x13] @currhp
-sub r0, #1
-strh r0, [r7, #4] @final damage
+	@check for Miracle
+	mov r0, r5 @defender data
+	add r0, #0x1E
+	mov r2, #0x0
+	ItemCheckLoop:
+		ldrb r1, [r0, r2]
+		cmp r1, #PrayerRingID
+		beq DoMiracle
+		add r2, #0x2
+		cmp r2, #0xA
+		blt ItemCheckLoop
+	b End
 
-End:
-pop {r4-r7}
-pop {r15}
+	DoMiracle:
+	ldr  r2,[r6]    
+	lsl  r1, r2, #0xD    
+	lsr  r1, r1, #0xD
+	mov  r0, #0x80
+	lsl  r0, #8 @defender skill
+	orr  r1, r0
+	ldr  r0, =#0xFFF80000
+	and  r0, r2    
+	orr  r0, r1     
+	str  r0, [r6]
+	@and set damage to currhp-1
+	ldrb r0, [r5, #0x13] @currhp
+	sub  r0, #1
+	strh r0, [r7, #4] @final damage
+
+	End:
+	pop {r4-r7}
+	pop {r15}
 
 .align
 .ltorg
--- Engine_Hacks/Gaiden/BattleMechanics/ItemSkills/SureShot.asm
+++ Engine_Hacks/Gaiden/BattleMechanics/ItemSkills/SureShot.asm
@@ -25,26 +25,25 @@ cmp r0, #LunaBowID
 bne End
 @if user has sure shot, check for proc rate
 
-	@ldrb r0, [r4, #0x15] @skill stat as activation rate
-	@mov r1, r4 @skill user
-	@blh d100Result
-	@cmp r0, #1
-	@bne End
-
 @if we proc, unset the miss flag.
-ldr     r2,[r6]    
-lsl     r1,r2,#0xD                @ 0802B42C 0351     
-lsr     r1,r1,#0xD                @ 0802B42E 0B49     
-mov     r0,#0x2          @miss flag     @ 0802B430 2002  
-mvn     r0, r0
-and     r1,r0            @unset it
-	@mov     r0, #0x40
-	@lsl     r0, #8           @0x4000, attacker skill @activated
-	@orr     r1, r0
-ldr     r0,=#0xFFF80000                @ 0802B434 4804     
-and     r0,r2                @ 0802B436 4010     
-orr     r0,r1                @ 0802B438 4308     
-str     r0,[r6]                @ 0802B43A 6018   
+ldr     r2, [r6]    
+lsl     r1, r2, #0xD    
+lsr     r1, r1, #0xD   
+mov     r0, #0x2         @miss flag 
+and     r0, r1
+cmp     r0, #0x0
+beq     DontActivate
+	mov     r0, #0x2
+	mvn     r0, r0
+	and     r1, r0           @unset it
+	mov     r0, #0x40
+	lsl     r0, #8      @0x4000, attacker skill activated
+	orr     r1, r0
+DontActivate:
+ldr     r0,=#0xFFF80000     
+and     r0,r2     
+orr     r0,r1     
+str     r0,[r6]   
 	@ldrb    r0, SureShotID
 	@strb    r0,[r6,#4] @save the thing
 
