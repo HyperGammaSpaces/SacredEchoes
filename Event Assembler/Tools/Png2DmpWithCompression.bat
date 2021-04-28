c0cc14e1b1a3fd8b6f16d04f019ea91ca2eb2eb4
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2021-04-28 12:35:02 -0700

    new mage mapsprite by Leif

--- Event Assembler/Tools/Png2DmpWithCompression.bat
+++ Event Assembler/Tools/Png2DmpWithCompression.bat
@@ -1,3 +1,6 @@
-SET Png2Dmp="%~p0\..\..\..\..\Event Assembler\Tools\Png2Dmp.exe"
+@echo off
 
-%Png2Dmp% "%~1" --lz77
+@rem call with palette output and compression
+"%~dp0Png2Dmp" "%~1" --lz77
+
+pause
