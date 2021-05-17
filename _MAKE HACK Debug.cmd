0efc5c60cb85307b1359fcbde161470b5541546f
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2021-05-17 12:50:44 -0700

    text updates and add debug buildfile

--- _MAKE HACK Debug.cmd
+++ _MAKE HACK Debug.cmd
@@ -2,7 +2,7 @@ set "textprocess=%~dp0Tools\TextProcess\text-process-classic"
 set "parsefile=%~dp0Event Assembler\Tools\ParseFile.exe"
 
 cd %~dp0
-copy "FE8_clean.gba" "SacredEchoes.gba"
+copy "FE8_clean.gba" "SacredEchoesDEBUG.gba"
 cd "%~dp0/Definitions"
 py MakeParseDefs.py "Portrait Definitions.event" ParseDefinitions.txt ../Text/ParseDefinitions.txt Portrait_
 cd "%~dp0Tables"
@@ -12,10 +12,10 @@ echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --depends
 cd "%~dp0Maps"
 echo: | (tmx2ea -s)
 cd "%~dp0Event Assembler"
-ColorzCore A FE8 "-output:%~dp0SacredEchoes.gba" "-input:%~dp0Buildfile.event" "--nocash-sym"
+ColorzCore A FE8 "-output:%~dp0SacredEchoesDEBUG.gba" "-input:%~dp0DebugBuildfile.event" "--nocash-sym"
 cd "%~dp0sym"
-java -jar %~dp0sym\SymCombo.jar "%~dp0SacredEchoes.sym" "Stan.sym"
+java -jar %~dp0sym\SymCombo.jar "%~dp0SacredEchoesDEBUG.sym" "Stan.sym"
 
 cd "%~dp0ups"
-ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0SacredEchoes.gba" -o "SacredEchoesNIGHTLY.ups"
+ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0SacredEchoesDEBUG.gba" -o "SacredEchoesDEBUG.ups"
 pause
