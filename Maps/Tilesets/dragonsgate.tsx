6cbf1068bebe47042158e66b84a76852d2f39f04
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2022-12-19 18:19:25 -0800

    text revisions + map updates + hard mode enemy loadouts

---
+++ Maps/Tilesets/dragonsgate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dragonsgate" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="dragonsgate.png" width="512" height="512"/>
+</tileset>
